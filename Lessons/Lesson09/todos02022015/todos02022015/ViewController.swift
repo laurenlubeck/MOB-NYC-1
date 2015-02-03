c5d7b5abf752d322753af9d286344ecc1001c4d1
Lauren Lubeck <NielsenLaurenLubeck@USLC02M18N5F5V8.local>
2015-02-02 21:10:05 -0500

    lesson

--- Lessons/Lesson09/todos02022015/todos02022015/ViewController.swift
+++ Lessons/Lesson09/todos02022015/todos02022015/ViewController.swift
@@ -13,13 +13,56 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // this is the traditional location for look and feel of an app
+        
+//        var container = UIView(Frame: CGRect(x: 0, y: 0, width: 500, height: 780)
+//            container.backgroundColor =
+        
+        var mainView = UIView(frame: CGRect(x: 0, y: 0, width: 500, height: 780))
+        mainView.backgroundColor = UIColor.grayColor()
+        
+        var userName = UITextField(frame: CGRect(x: 50, y: 100, width: 250, height: 30))
+        userName.placeholder = "user name"
+        
+    
+        var password = UITextField(frame: CGRect(x: 50, y: 175, width: 250, height: 30))
+         password.placeholder = "password"
+        
+        var loginButton = UIButton(frame: CGRect(x: 50, y: 250, width: 250, height: 30))
+        loginButton.setTitle("Log in", forState:.Normal)
+     
+        loginButton.addTarget(self,action: "tappedButton",forControlEvents: UIControlEvents.TouchUpInside)
+        
+        loginButton.addTarget(self,action: "jumpingButton",forControlEvents: UIControlEvents.TouchUpInside)
+        
+        var label = UILabel(frame: CGRect(x: 50, y: 65, width: 200, height: 60))
+        label.text = "Todo Feb 2, 2015"
+        label.textColor = UIColor.whiteColor()
+        
+            
+    // add as a subclass of the main view
+        self.view.addSubview(mainView)
+        mainView.addSubview(userName)
+        mainView.addSubview(loginButton)
+        mainView.addSubview(password)
+        mainView.addSubview(label)
+        
     }
-
+    
+        func tappedButton() {
+            println("it works!")
+        }
+    
+ //   func jumpingButton(){
+     //
+   // }
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
