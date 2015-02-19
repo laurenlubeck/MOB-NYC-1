a9ee39bc46a681caf092665bef09cc7718781b3f
Lauren Lubeck <NielsenLaurenLubeck@USLC02M18N5F5V8.local>
2015-02-18 21:27:55 -0500

    no message

--- Class Notes/practice feb 18/practice feb 18/ViewController.swift
+++ Class Notes/practice feb 18/practice feb 18/ViewController.swift
@@ -10,16 +10,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var backgroundSwitch: UISwitch!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        // set up for inital loading, link to function
+        self.backgroundSwitch.addTarget(self, action: "didChangeSwitch", forControlEvents: UIControlEvents.ValueChanged)
+        
+        
+        // set default for the switch 3
+        self.backgroundSwitch.on = NSUserDefaults.standardUserDefaults().boolForKey("switchIsOn")
 
+        
+        // manually refresh label when view loads
+        didChangeSwitch()
+
+        
+        // to persist an array of something and call it from a plist
+        if let path = NSBundle.mainBundle().pathForResource("PropertyList", ofType: "plist") {
+            var array = NSArray(contentsOfFile: path)
+            println(array)
+            array?.writeToFile(path, atomically: true)
+        }
+    }
+    
+    
+    
+    func didChangeSwitch() {
+        // print line stating the background switch is on or off, will appear true for on and false for off
+        // println(self.backgroundSwitch.on)
+        
+        
+        if self.backgroundSwitch.on {
+            // if on make label large font
+            self.label.font = UIFont.systemFontOfSize(30)
+            
+        } else {
+            // if off make label small font
+            self.label.font = UIFont.systemFontOfSize(10)
+            
+        }
+        
+        // to store a boolean value of weather switch is on
+        NSUserDefaults.standardUserDefaults().setBool(self.backgroundSwitch.on, forKey: "switchIsOn")
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
---
+++ Class Notes/practice feb 18/practice feb 18/todo feb 18.swift
@@ -0,0 +1,9 @@
+//
+//  todo feb 18.swift
+//  practice feb 18
+//
+//  Created by Lauren Lubeck  on 2/18/15.
+//  Copyright (c) 2015 lauren Lubeck. All rights reserved.
+//
+
+import Foundation
