e4ca678fe76a57053dd19a445dbb26eee9b4d188
Lauren Lubeck <NielsenLaurenLubeck@USLC02M18N5F5V8.local>
2015-02-18 19:08:36 -0500

    Initial Commit

---
+++ Class Notes/practice feb 18/practice feb 18/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  practice feb 18
+//
+//  Created by Lauren Lubeck  on 2/18/15.
+//  Copyright (c) 2015 lauren Lubeck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
